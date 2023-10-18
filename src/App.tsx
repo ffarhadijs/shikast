d0b599d6385955176832d37da312288a790440b9
farhad farhadi <rb0214192@gmail.com>
2023-10-19 00:33:42 +0330

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "@mantine/core/styles.css";
+import { Button, MantineProvider } from "@mantine/core";
+
+function App() {
+  return <MantineProvider></MantineProvider>;
+}
+
+export default App;
